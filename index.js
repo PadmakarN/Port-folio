892c2a0450f22fb92919ddba02ae7c7378bad196
PadmakarNarwade <76721992+PadmakarN@users.noreply.github.com>
2025-06-06 14:12:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-test
+function getexpdays(fromdatestr){
+const fromDate =new Date(fromdatestr);
+const toDate= new Date();
+
+let years=toDate.getFullYear()-fromDate.getFullYear();
+let months=toDate.getMonth()-fromDate.getMonth();
+let days=toDate.getDate()-fromDate.getDate();
+
+if (days<0){
+months-=1;
+const prevmonth=new Date(toDate.getFullyear(),toDate.getMonth(),0);
+days+=prevMonth.getDate();
+}
+if(month <0){
+year -=1;
+months +=12;}
+
+return {years,month,days};
+}
